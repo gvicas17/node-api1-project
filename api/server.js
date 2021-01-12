3b0e0ddbc3fdf234cbe67896b36ee924c139000d
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-11 21:03:57 -0500

    created get requests and post requests

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const server = express()
+
+server.use(express.json())
+
+server.get('/api/users', (req, res) => {
+    
+})
+server.get('/api/users/:id', (req, res) => {
+    
+})
+server.post('/api/users', (req, res) => {
+    
+})
+server.delete('/api/users/:id', (req, res) => {
+    
+})
+server.put('/api/users/:id', (req, res) => {
+    
+})
